886fd078090bd1c428359bdc736236328c2fc4c6
Lucas Kahl <lucaschangkahl@gmail.com>
2018-09-20 08:31:50 -0300

    Adding POST Method lesson

--- routes/users.js
+++ routes/users.js
@@ -13,12 +13,8 @@ module.exports = (app)=> {
             });
     });
     
-    app.get('/users/admin', (req, res) => {
-    
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: []
-        });
+    app.post('/users', (req, res) => {
+
+        res.json(req.body);
     });
 }
